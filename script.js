de2794aa705d4283e9297bd1df45994d247e9d20
FabeehaAhmed <49812761+FabeehaAhmed@users.noreply.github.com>
2019-07-11 18:04:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,211 @@
+let techKaro = {
+    courseName: "Tech Karo",
+    students: [
+        {
+            name: "Fabeeha",
+                fatherName: "Ahmed",
+            fatherProfession: "Teaching",
+                hobbies: "Gardening",
+            contactNo: 0321464858,
+                dateOfBirth: 20/8/1998,
+            gender: "Female",
+                group: "A",
+            emailAddress: "fabeehaahmed01@outlook.com",
+                qualification: "Inter",
+            sibblings: "2",
+                cnic: 42101-2378375389-9,
+            ambassadorName: "Arsala"
+
+
+        },
+        {
+            name: "Pervaiz",
+                fatherName: "Fareed",
+            fatherProfession: "cashier",
+                hobbies: "LearningBooks",
+            contactNo: "0347808501",
+                dateOfBirth: "06/07/1996",
+            gender: "Male",
+                group: "B",
+            emailAddress: "pervaizshah083@gmail.com",
+                qualification: "softwareEngineer",
+            sibblings: "",
+                cnic: "42401-677853576-878",
+            ambassadorName: "Noreen Naz",
+
+
+                
+
+        }
+        {
+            name: "Erum",
+                fatherName: "Razakhan",
+            fatherProfession: 
+                hobbies:
+            contactNo:
+                dateOfBirth:
+            gender:
+                group:
+            emailAddress:
+                qualification:
+            sibblings:
+                cnic:
+            ambassadorName:
+            {
+                name:
+                    fatherName:
+                fatherProfession:
+                    hobbies:
+                contactNo:
+                    dateOfBirth:
+                gender:
+                    group:
+                emailAddress:
+                    qualification:
+                sibblings:
+                    cnic:
+                ambassadorName:
+            },
+            {
+
+            name:
+                fatherName:
+            fatherProfession:
+                hobbies:
+            contactNo:
+                dateOfBirth:
+            gender:
+                group:
+            emailAddress:
+                qualification:
+            sibblings:
+                cnic:
+            ambassadorName:
+            },
+            {
+            name:
+                fatherName:
+            fatherProfession:
+                hobbies:
+            contactNo:
+                dateOfBirth:
+            gender:
+                group:
+            emailAddress:
+                qualification:
+            sibblings:
+                cnic:
+            ambassadorName:
+            },
+            
+            {
+            name:
+                fatherName:
+            fatherProfession:
+                hobbies:
+            contactNo:
+                dateOfBirth:
+            gender:
+                group:
+            emailAddress:
+                qualification:
+            sibblings:
+                cnic:
+            ambassadorName:
+            },
+            {
+            name:
+                fatherName:
+            fatherProfession:
+                hobbies:
+            contactNo:
+                dateOfBirth:
+            gender:
+                group:
+            emailAddress:
+                qualification:
+            sibblings:
+                cnic:
+            ambassadorName:
+        },
+
+            {
+
+            name:
+                fatherName:
+            fatherProfession:
+                hobbies:
+            contactNo:
+                dateOfBirth:
+            gender:
+                group:
+            emailAddress:
+                qualification:
+            sibblings:
+                cnic:
+            ambassadorName:
+            },
+                  {
+            name:
+                fatherName:
+            fatherProfession:
+                hobbies:
+            contactNo:
+                dateOfBirth:
+            gender:
+                group:
+            emailAddress:
+                qualification:
+            sibblings:
+                cnic:
+            ambassadorName:
+                  }  
+    ]
+batchNo:
+faculty:
+ambassadors:
+duration:
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
